0509401c75a96c70a0890ab175996be1e3e95b2d
0nlyoung7 <0nlyoung7@gmail.com>
2017-02-10 17:29:13 +0900

    Add SSLSecurity

--- SocketIO/SocketIOClientOption.h
+++ SocketIO/SocketIOClientOption.h
@@ -1,11 +1,40 @@
 #import <Foundation/Foundation.h>
+#import <Security/Security.h>
+
+typedef NS_ENUM(NSInteger, SocketIOClientStatus) {
+    NotConnected = 0,
+    Disconnected = 1,
+    Connecting =2,
+    Connected = 3
+};
 
 @interface SocketIOClientOption : NSObject
 
 @property (nonatomic, assign) NSInteger placeholders;
 
-@property (nonatomic, copy)   NSMutableDictionary *connectParams;
-@property (nonatomic, copy)    NSMutableDictionary *cookies;
+@property (nonatomic, strong)   NSMutableDictionary *connectParams;
+@property (nonatomic, strong)   NSMutableDictionary *cookies;
 @property (nonatomic, assign) BOOL doubleEncodeUTF8;
+@property (nonatomic, copy)   NSMutableDictionary *extraHeaders;
+@property (nonatomic, assign) BOOL forceNew;
+@property (nonatomic, assign) BOOL forcePolling;
+@property (nonatomic, assign) BOOL forceWebsockets;
+
+//@property (nonatomic, assign) DispatchQueue handleQueue;
+
+@property (nonatomic, assign) BOOL log;
+
+@property (nonatomic, copy)   NSString *nsp;
+@property (nonatomic, copy)   NSString *path;
+@property (nonatomic, assign) BOOL reconnects;
+@property (nonatomic, assign) NSInteger reconnectAttempts;
+@property (nonatomic, assign) NSInteger reconnectWait;
+
+@property (nonatomic, assign) BOOL secure;
+//@property (nonatomic, copy) SSLSecurity *security;
+@property (nonatomic, assign) BOOL selfSigned;
+
+//@property (nonatomic, copy) NSURLSessionDelegate *sessionDelegate;
+@property (nonatomic, assign) BOOL voipEnabled;
 
 @end
