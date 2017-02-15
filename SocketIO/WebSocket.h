c521555490f660525e596d7a11c5c429660fdeca
0nlyoung7 <0nlyoung7@gmail.com>
2017-02-15 18:09:26 +0900

    Add WebSocket

---
+++ SocketIO/WebSocket.h
@@ -0,0 +1,75 @@
+#import <Foundation/Foundation.h>
+#import <Security/Security.h>
+
+#import "SSLSecurity.h"
+
+@protocol WebSocketDelegate <NSObject>
+
+@end
+
+FOUNDATION_EXPORT NSString *const WebsocketDidConnectNotification;
+FOUNDATION_EXPORT NSString *const WebsocketDidDisconnectNotification;
+FOUNDATION_EXPORT NSString *const WebsocketDisconnectionErrorKeyName;
+
+@interface WebSocket : NSObject <NSStreamDelegate>
+    
+typedef NS_ENUM(uint8_t, OpCode) {
+    ContinueFrame = 0x0,
+    TextFrame = 0x1,
+    BinaryFrame = 0x2,
+    ConnectionClose = 0x8,
+    Ping = 0x8,
+    Pong = 0xA
+};
+
+typedef NS_ENUM(uint16_t, CloseCode) {
+    Normal = 1000,
+    GoingAway = 1001,
+    ProtocolError = 1002,
+    ProtocolUnhandledType = 1003,
+    NoStatusReceived = 1005,
+    Encoding = 1007,
+    PolicyViolated = 1008,
+    MessageTooBig = 1009
+};
+
+typedef NS_ENUM(uint16_t, InternalErrorCode) {
+    OutputStreamWriteError = 1
+};
+
+@property(nonatomic,weak, nullable)id<WebSocketDelegate>delegate;
+@property(nonatomic, readonly, nonnull) NSURL *url;
+
+- (nonnull instancetype)initWithURL:(nonnull NSURL *)url protocols:(nullable NSArray*)protocols;
+
+- (void)connect;
+
+- (void)disconnect;
+
+- (void)writeData:(nonnull NSData*)data;
+
+- (void)writeString:(nonnull NSString*)string;
+
+- (void)writePing:(nonnull NSData*)data;
+
+- (void)addHeader:(nonnull NSString*)value forKey:(nonnull NSString*)key;
+
+@property(nonatomic, strong, nullable)void (^onConnect)(void);
+
+@property(nonatomic, strong, nullable)void (^onDisconnect)(NSError*_Nullable);
+
+@property(nonatomic, strong, nullable)void (^onText)(NSString*_Nullable);
+
+@property(nonatomic, strong, nullable)void (^onData)(NSData*_Nullable);
+
+@property(nonatomic, strong, nullable)void (^onPong)(NSData*_Nullable);
+
+@property(nonatomic, strong, nullable)NSMutableDictionary *headers;
+@property(nonatomic, assign)BOOL voipEnabled;
+@property(nonatomic, assign)BOOL disableSSLCertValidation;
+@property(nonatomic, strong, nullable)SSLSecurity *security;
+@property(nonatomic, assign, readonly)BOOL isConnected;
+
+@property(nonatomic, strong, nullable)dispatch_queue_t queue;
+
+@end
---
+++ SocketIO/WebSocket.m
@@ -0,0 +1,86 @@
+#import "WebSocket.h"
+
+NSString *const WebsocketDidConnectNotification = @"WebsocketDidConnectNotification";
+NSString *const WebsocketDidDisconnectNotification = @"WebsocketDidDisconnectNotification";
+NSString *const WebsocketDisconnectionErrorKeyName = @"WebsocketDisconnectionErrorKeyName";
+
+static NSString *const ErrorDomain = @"WebSocket";
+
+@interface WSResponse : NSObject
+
+@property(nonatomic, assign)BOOL isFin;
+@property(nonatomic, assign)OpCode code;
+@property(nonatomic, assign)NSInteger bytesLeft;
+@property(nonatomic, assign)NSInteger frameCount;
+@property(nonatomic, strong)NSMutableData *buffer;
+
+@end
+
+@interface WebSocket () <NSStreamDelegate>
+
+@property(nonatomic, strong, nonnull)NSURL *url;
+@property(nonatomic, strong, null_unspecified)NSInputStream *inputStream;
+@property(nonatomic, strong, null_unspecified)NSOutputStream *outputStream;
+@property(nonatomic, strong, null_unspecified)NSOperationQueue *writeQueue;
+@property(nonatomic, assign)BOOL isRunLoop;
+@property(nonatomic, strong, nonnull)NSMutableArray *readStack;
+@property(nonatomic, strong, nonnull)NSMutableArray *inputQueue;
+@property(nonatomic, strong, nullable)NSData *fragBuffer;
+@property(nonatomic, strong, nullable)NSMutableDictionary *headers;
+@property(nonatomic, strong, nullable)NSArray *optProtocols;
+@property(nonatomic, assign)BOOL isCreated;
+@property(nonatomic, assign)BOOL didDisconnect;
+@property(nonatomic, assign)BOOL certValidated;
+
+@end
+
+//Constant Header Values.
+NS_ASSUME_NONNULL_BEGIN
+static NSString *const headerWSUpgradeName     = @"Upgrade";
+static NSString *const headerWSUpgradeValue    = @"websocket";
+static NSString *const headerWSHostName        = @"Host";
+static NSString *const headerWSConnectionName  = @"Connection";
+static NSString *const headerWSConnectionValue = @"Upgrade";
+static NSString *const headerWSProtocolName    = @"Sec-WebSocket-Protocol";
+static NSString *const headerWSVersionName     = @"Sec-Websocket-Version";
+static NSString *const headerWSVersionValue    = @"13";
+static NSString *const headerWSKeyName         = @"Sec-WebSocket-Key";
+static NSString *const headerOriginName        = @"Origin";
+static NSString *const headerWSAcceptName      = @"Sec-WebSocket-Accept";
+NS_ASSUME_NONNULL_END
+
+//Class Constants
+static char CRLFBytes[] = {'\r', '\n', '\r', '\n'};
+static int BUFFER_MAX = 4096;
+
+// This get the correct bits out by masking the bytes of the buffer.
+static const uint8_t FinMask             = 0x80;
+static const uint8_t OpCodeMask          = 0x0F;
+static const uint8_t RSVMask             = 0x70;
+static const uint8_t MaskMask            = 0x80;
+static const uint8_t PayloadLenMask      = 0x7F;
+static const size_t  MaxFrameSize        = 32;
+
+#define HttpSwitchProtocolCode 101
+
+@implementation WebSocket
+{
+}
+
+- (instancetype)initWithURL:(NSURL *)url protocols:(NSArray*)protocols
+{
+    if(self = [super init]) {
+        self.certValidated = NO;
+        self.voipEnabled = NO;
+        self.selfSignedSSL = NO;
+        self.queue = dispatch_get_main_queue();
+        self.url = url;
+        self.readStack = [NSMutableArray new];
+        self.inputQueue = [NSMutableArray new];
+        self.optProtocols = protocols;
+    }
+    
+    return self;
+}
+
+@end
