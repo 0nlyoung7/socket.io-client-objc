8fdce26336970dbf2a493430e633fa54930f41c3
0nlyoung7 <0nlyoung7@gmail.com>
2017-03-09 19:03:11 +0900

    Add function in SocketEngine

--- SocketIO/SocketEngine.m
+++ SocketIO/SocketEngine.m
@@ -1,5 +1,6 @@
 #import <Foundation/Foundation.h>
 #import "SocketEngine.h"
+#import "SocketEnginePacketType.h"
 
 @implementation SocketEngine
 {
@@ -11,7 +12,93 @@
     self.client = client;
     self.url = url;
     
+    for( NSString* key in config ){
+        if(  [config[key] isEqual:@"ConnectParams"] ){
+       
+        }
+    }
+    
     return self;
+}
+
+
+- (void) dealloc {
+    [self setClosed:TRUE];
+    [self stopPolling];
+}
+
+- (void) checkAndHandleEngineError:(NSString*) str{
+    NSDictionary  *dict = [self toNSDictionary:str];
+    if( dict[@"message"] ){
+        [self didError:dict[@"message"]];
+    }
+}
+
+- (void) didError:(NSString*) reason {
+    if( self.client ){
+        [self.client engineDidError:reason];
+        [self disconnect:reason];
+    }
+}
+
+- (void) disconnect:(NSString*) reason {
+    if( !self.connected ){
+        [self closeOutEngine:reason];
+        return;
+    }
+    
+    if( self.closed ){
+        [self closeOutEngine:reason];
+        return;
+    }
+    
+    if( self.websocket ){
+        // sendWebSocketMessage
+        [self closeOutEngine:reason];
+    } else {
+        // disconnectPolling
+    }
+}
+
+- (void) disconnectPolling:(NSString*) reason {
+    dispatch_async(self.emitQueue ,^{
+        NSString *typeStr = [NSString stringWithFormat: @"%ld", (long)Close];
+        [self.postWait addObject:typeStr];
+        
+        NSMutableURLRequest  *req = [self createRequestForPostWithPostWait];
+        [self doRequest:req callbackWith:nil];
+        [self closeOutEngine:reason];
+    });
+}
+
+- (void) closeOutEngine:(NSString*) reason{
+    [self setSid:@""];
+    [self setClosed:TRUE];
+    [self setInvalidated:TRUE];
+    [self setConnected:FALSE];
+    
+    if( self.ws ){
+        [self.ws disconnect];
+        [self stopPolling];
+        if( self.client ){
+            [self.client engineDidClose:reason];
+        }
+    }
+}
+
+- (NSDictionary*) toNSDictionary:(NSString*) str{
+    
+    NSData *data = [str dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:NO];
+    
+    NSError *e = nil;
+    NSDictionary *json = [NSJSONSerialization JSONObjectWithData: data options: NSJSONReadingMutableContainers error: &e];
+    
+    if (!json) {
+        NSLog(@"Error parsing JSON: %@", e);
+        return nil;;
+    }
+    
+    return json;
 }
 
 @end
--- SocketIO/SocketEnginePollable.h
+++ SocketIO/SocketEnginePollable.h
@@ -5,15 +5,19 @@
 
 @property (nonatomic, readonly) BOOL invalidated;
 
-@property (nonatomic, copy) NSMutableArray<NSString *> *postWait;
-@property (nonatomic, copy, readonly) NSURLSession *session;
+@property (nonatomic, copy, nullable) NSMutableArray<NSString *> *postWait;
+@property (nonatomic, copy, readonly, nullable) NSURLSession *session;
 
 @property (nonatomic) BOOL waitingForPoll;
 @property (nonatomic) BOOL waitingForPost;
 
 -(void) doPoll;
 
--(void) sendPollMessage:(NSString*) message type:(SocketEnginePacketType)type withData:(NSArray<NSData*> *) datas;
+-(NSMutableURLRequest* _Nonnull) createRequestForPostWithPostWait;
+
+-(void) doRequest:(NSMutableURLRequest* _Nullable) req callbackWith:(void ( ^ _Nullable )(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)) callback;
+
+-(void) sendPollMessage:(NSString* _Nullable) message type:(SocketEnginePacketType)type withData:(NSArray<NSData*> * _Nullable) datas;
 
 -(void) stopPolling;
 
